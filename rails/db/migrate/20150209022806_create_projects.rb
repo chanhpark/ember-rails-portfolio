7a0cb0c2feab0f477daef5357931f437efbbc4fd
chanhpark <chanhyun.park1211@gmail.com>
2015-02-08 21:43:29 -0500

    ember-cli-pretender and backend rails api

---
+++ rails/db/migrate/20150209022806_create_projects.rb
@@ -0,0 +1,10 @@
+class CreateProjects < ActiveRecord::Migration
+  def change
+    create_table :projects do |t|
+      t.string :name, null: false
+      t.string :app_url, null: false
+      t.text :description, null: false
+      t.string :repo_url, null: false
+    end
+  end
+end
